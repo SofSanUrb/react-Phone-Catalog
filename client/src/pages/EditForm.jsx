75b1c4871388256264d8d52f38f85351915b51af
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-28 22:44:29 +0200

    edit form handle Change

--- client/src/pages/EditForm.jsx
+++ client/src/pages/EditForm.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
+import config from "../config";
+import axios from "axios";
 
 export default function EditForm(props) {
-  const { onAdd, error } = props;
+  const [phone, setPhone] = useState({});
+  useEffect(() => {
+    axios
+      .get(`${config.API_URL}/api/phones/${props.match.params.phoneId}`)
+      .then((response) => setPhone(response.data))
+      .catch(() => console.log("Fetching failed"));
+  }, [props.match.params.phoneId]);
+
+  const handleChangeForm = (event) => setPhone({
+    ...phone,
+    [event.currentTarget.name] : event.currentTarget.value
+})
+
   return (
-    <Form onSubmit={onAdd}>
+    <Form >
       <Form.Group>
         <Form.Label>Name</Form.Label>
-        <Form.Control name="name" type="text" placeholder="Iphone xx" />
+        <Form.Control name="name" type="text" value={phone.name} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Manufacturer</Form.Label>
-        <Form.Control name="manufacturer" type="text" placeholder="Apple" />
+        <Form.Control name="manufacturer" type="text" value={phone.manufacturer} onChange={handleChangeForm} />
       </Form.Group>
       <Form.Group>
         <Form.Label>Description</Form.Label>
-        <Form.Control name="description" type="text" />
+        <Form.Control name="description" value={phone.description} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.Label>color</Form.Label>
-        <Form.Control name="color" type="text" placeholder="black" />
+        <Form.Control name="color" type="text" value={phone.color} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Price (€)</Form.Label>
-        <Form.Control name="price" type="number" placeholder="600" />
+        <Form.Control name="price" type="number" value={phone.price} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.File
@@ -34,19 +48,17 @@ export default function EditForm(props) {
       </Form.Group>
       <Form.Group>
         <Form.Label>Screen</Form.Label>
-        <Form.Control name="screen" type="text" placeholder="4,7 inch IPS" />
+        <Form.Control name="screen" type="text" value={phone.screen} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Processor</Form.Label>
-        <Form.Control name="processor" type="text" placeholder="A10 Fusion" />
+        <Form.Control name="processor" type="text" value={phone.processor} onChange={handleChangeForm}/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Ram</Form.Label>
-        <Form.Control name="ram" type="number" placeholder="2" />
+        <Form.Control name="ram" type="number" value={phone.ram} onChange={handleChangeForm}/>
       </Form.Group>
-      {error ? (
-        <p className="errorMessage">{error.errorMessage}</p>
-      ) : null}
+      {/* {error ? <p className="errorMessage">{error.errorMessage}</p> : null} */}
       <Button variant="primary" type="submit">
         Submit
       </Button>
