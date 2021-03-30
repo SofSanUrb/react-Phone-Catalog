ef216a1640215df898be77674a28159643cd6a7d
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-30 20:33:34 +0200

    Phone Detail loading

--- client/src/pages/PhoneDetail.jsx
+++ client/src/pages/PhoneDetail.jsx
@@ -8,58 +8,77 @@ import axios from "axios";
 
 export default function PhoneDetail(props) {
   const [phone, setPhone] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
   useEffect(() => {
     axios
-      .get(`${config.API_URL}/api/phones/${props.match.params.phoneId}`, {withCredentials:true})
-      .then((response) => setPhone(response.data))
+      .get(`${config.API_URL}/api/phones/${props.match.params.phoneId}`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        setPhone(response);
+        setLoading(false);
+      })
       .catch(() => console.log("Fetching failed"));
   }, [props.match.params.phoneId]);
 
   const handleDeletePhone = (id) => {
-    axios.delete(`${config.API_URL}/api/delete/${id}`, {withCredentials:true})
-    .then(() => {
-      props.history.push('/')
-  })
-  .catch(err => console.log(err))
-  }
+    axios
+      .delete(`${config.API_URL}/api/delete/${id}`, { withCredentials: true })
+      .then(() => {
+        props.history.push("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-    <Helmet>
-      <title>Phone - Details</title>
-    </Helmet>
-    <div className="card-column">
-        <div className="phone-detail">
-          <img
-            className="phone-index-picture"
-            src={phone.imageFileName}
-            alt="phone-front"
-          />
-          <div className="detail-info">
-            <h3>{phone.name}</h3>
+      <Helmet>
+        <title>Phone - Details</title>
+      </Helmet>
+      {isLoading ? (
+        <div className="spinner-column">
+          <Spinner className="my-spinner" animation="grow" />
+        </div>
+      ) : (
+        <div className="card-column">
+          <div className="phone-detail">
+            <img
+              className="phone-index-picture"
+              src={phone.imageFileName}
+              alt="phone-front"
+            />
+            <div className="detail-info">
+              <h3>{phone.name}</h3>
 
-            <div className="phone-details-row">
-              <p>Manufacturer: {phone.manufacturer}</p>
-              <p>Color: {phone.color}</p>
+              <div className="phone-details-row">
+                <p>Manufacturer: {phone.manufacturer}</p>
+                <p>Color: {phone.color}</p>
+              </div>
+
+              <p className="text-left text-italic">"{phone.description}"</p>
+
+              <div className="phone-details-row">
+                <p>{phone.screen} Screen</p>
+                <p>{phone.processor} Processor</p>
+                <p>RAM: {phone.ram}GB</p>
+              </div>
+              <h4>Price: {phone.price} €</h4>
             </div>
-
-            <p className="text-left text-italic">"{phone.description}"</p>
-
-            <div className="phone-details-row">
-              <p>{phone.screen} Screen</p>
-              <p>{phone.processor} Processor</p>
-              <p>RAM: {phone.ram}GB</p>
+            <div className="phone-details-row detail-buttons">
+              <Link to={`/edit/${phone._id}`}>
+                <Button variant="primary">Edit</Button>
+              </Link>
+              <Button
+                variant="light"
+                onClick={() => handleDeletePhone(phone._id)}
+              >
+                Delete
+              </Button>
             </div>
-            <h4>Price: {phone.price} €</h4>
-          </div>
-          <div className="phone-details-row detail-buttons">
-            <Link to={`/edit/${phone._id}`}>
-              <Button variant="primary">Edit</Button>
-            </Link>
-            <Button variant="light" onClick={() => handleDeletePhone(phone._id)}>Delete</Button>
           </div>
         </div>
-    </div>
+      )}
     </>
   );
 }
